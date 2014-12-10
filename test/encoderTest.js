7fedde80c2ff29d626767fc12943a6baa0ac8988
Don Coleman <dcoleman@chariotsolutions.com>
2014-12-09 20:39:37 -0800

    update code to meet the revised uribeacon encoding https://github.com/google/uribeacon/tree/cefc17f4f11a32eec232dc73fc683d9547ea742d/specification

--- test/encoderTest.js
+++ test/encoderTest.js
@@ -30,23 +30,12 @@ describe('URI Encoder', function () {
             assert.equal(3, encoded[0]); // prefix
         });
 
-        it('tel', function () {
-            var encoded = uriBeacon.encode("tel:+1-215-555-1212");
+        it('urn:uuid:', function () {
+            var encoded = uriBeacon.encode("urn:uuid:B1E13D51-5FC9-4D5B-902B-AB668DD54981");
             assert.equal(4, encoded[0]); // prefix
         });
 
-        it('mailto', function () {
-            var encoded = uriBeacon.encode("mailto:foo@example.com");
-            assert.equal(5, encoded[0]); // prefix
-        });
-
-        it('geo', function () {
-            var encoded = uriBeacon.encode("geo:40.134327,-75.205673");
-            assert.equal(6, encoded[0]); // prefix
-        });
-
     });
-
 
     it('should use first protocol match', function () {
 
@@ -62,14 +51,34 @@ describe('URI Encoder', function () {
 
     });
 
-    it('should handle unknown protocols', function () {
+    describe('should handle unknown protocols', function () {
 
-        var encoded = uriBeacon.encode("sms:2153581780");
-        assert.equal('s'.charCodeAt(0), encoded[0]);
-        assert.equal('m'.charCodeAt(0), encoded[1]);
-        assert.equal('s'.charCodeAt(0), encoded[2]);
-        assert.equal(':'.charCodeAt(0), encoded[3]);
-        assert.equal(14, encoded.length);
+        it('sms', function() {
+            var encoded = uriBeacon.encode("sms:2153581780");
+            assert.equal('s'.charCodeAt(0), encoded[0]);
+            assert.equal('m'.charCodeAt(0), encoded[1]);
+            assert.equal('s'.charCodeAt(0), encoded[2]);
+            assert.equal(':'.charCodeAt(0), encoded[3]);
+            assert.equal(14, encoded.length);
+        });
+
+        it('tel', function () {
+            var uri = "tel:+1-215-555-1212";
+            var encoded = uriBeacon.encode(uri);
+            assert.equal(uri.length, encoded.length);
+        });
+
+        it('mailto', function () {
+            var uri = "mailto:foo@example.com";
+            var encoded = uriBeacon.encode(uri);
+            assert.equal(uri.length - 3, encoded.length); // .com is substituted
+        });
+
+        it('geo', function () {
+            var uri = "geo:40.134327,-75.205673";
+            var encoded = uriBeacon.encode(uri);
+            assert.equal(uri.length, encoded.length);
+        });
 
     });
 
@@ -86,10 +95,66 @@ describe('URI Encoder', function () {
             assert.equal(8, encoded[3]); // domain
         });
 
-        it('net', function () {
+        it('edu', function () {
             var encoded = uriBeacon.encode("foo.edu");
             assert.equal(9, encoded[3]); // domain
         });
+
+        it('net', function () {
+            var encoded = uriBeacon.encode("foo.net");
+            assert.equal(10, encoded[3]); // domain
+        });
+
+        it('info', function () {
+            var encoded = uriBeacon.encode("foo.info");
+            assert.equal(11, encoded[3]); // domain
+        });
+
+        it('biz', function () {
+            var encoded = uriBeacon.encode("foo.biz");
+            assert.equal(12, encoded[3]); // domain
+        });
+
+        it('gov', function () {
+            var encoded = uriBeacon.encode("foo.gov");
+            assert.equal(13, encoded[3]); // domain
+        });
+
+        it('com/', function () {
+            var encoded = uriBeacon.encode("foo.com/");
+            assert.equal(0, encoded[3]); // domain
+        });
+
+        it('org/', function () {
+            var encoded = uriBeacon.encode("foo.org/");
+            assert.equal(1, encoded[3]); // domain
+        });
+
+        it('edu/', function () {
+            var encoded = uriBeacon.encode("foo.edu/");
+            assert.equal(2, encoded[3]); // domain
+        });
+
+        it('net/', function () {
+            var encoded = uriBeacon.encode("foo.net/");
+            assert.equal(3, encoded[3]); // domain
+        });
+
+        it('info/', function () {
+            var encoded = uriBeacon.encode("foo.info/");
+            assert.equal(4, encoded[3]); // domain
+        });
+
+        it('biz/', function () {
+            var encoded = uriBeacon.encode("foo.biz/");
+            assert.equal(5, encoded[3]); // domain
+        });
+
+        it('gov/', function () {
+            var encoded = uriBeacon.encode("foo.gov/");
+            assert.equal(6, encoded[3]); // domain
+        });
+
 
     });
 
@@ -103,17 +168,32 @@ describe('URI Encoder', function () {
 
     });
 
+    it('should handle trailing slash', function () {
+
+        // no trailing slash
+        var encoded = uriBeacon.encode("http://www.chariotsolutions.com/");
+        assert.equal(0, encoded[0]); // prefix
+        assert.equal(0, encoded[17]); // domain
+        assert.equal(18, encoded.length);
+
+        // trailing slash
+        encoded = uriBeacon.encode("http://www.chariotsolutions.com/");
+        assert.equal(0, encoded[0]); // prefix
+        assert.equal(0, encoded[17]); // domain
+        assert.equal(18, encoded.length);
+
+    });
+
     it('should handle url path', function () {
 
         // this is probably too long
         var encoded = uriBeacon.encode("http://www.chariotsolutions.com/foo");
         assert.equal(0, encoded[0]); // prefix
-        assert.equal(7, encoded[17]); // domain
-        assert.equal('/'.charCodeAt(0), encoded[18]);
-        assert.equal('f'.charCodeAt(0), encoded[19]);
+        assert.equal(0, encoded[17]); // domain
+        assert.equal('f'.charCodeAt(0), encoded[18]);
+        assert.equal('o'.charCodeAt(0), encoded[19]);
         assert.equal('o'.charCodeAt(0), encoded[20]);
-        assert.equal('o'.charCodeAt(0), encoded[21]);
-        assert.equal(22, encoded.length);
+        assert.equal(21, encoded.length);
 
     });
 
@@ -121,6 +201,36 @@ describe('URI Encoder', function () {
 
         var encoded = uriBeacon.encode("qwerty");
         assert.equal(6, encoded.length);
+
+    });
+
+    describe('should encode UUIDs', function() {
+
+        it('with dashes', function() {
+            var encoded = uriBeacon.encode("urn:uuid:B1E13D51-5FC9-4D5B-902B-AB668DD54981");
+            assert.equal(4, encoded[0]); // prefix
+            assert.equal(17, encoded.length);
+        });
+
+        it('without dashes', function() {
+            var encoded = uriBeacon.encode("urn:uuid:B1E13D515FC94D5B902BAB668DD54981");
+            assert.equal(4, encoded[0]); // prefix
+            assert.equal(17, encoded.length);
+        });
+
+        it('too short', function() {
+            var encoded = uriBeacon.encode("urn:uuid:B1E13D51");
+            assert.equal(4, encoded[0]); // prefix
+            assert.equal(0, encoded[15]);
+            assert.equal(0, encoded[16]);
+            assert.equal(17, encoded.length);
+        });
+
+        it('too long', function() {
+            var encoded = uriBeacon.encode("urn:uuid:B1E13D51-5FC9-4D5B-902B-AB668DD54981999999999999999999999999");
+            assert.equal(4, encoded[0]); // prefix
+            assert.equal(17, encoded.length);
+        });
 
     });
 
