7362f9d0c94f1d12e0dff4a26f33a2639c5a9148
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-15 14:53:01 -0400

    update tests for #8

--- test/advertisementTest.js
+++ test/advertisementTest.js
@@ -5,46 +5,50 @@ var defaultTxPowerLevel = 0xEB; // -21dBm
 describe('Advertisement Buffer', function () {
 
     it('should match known message', function () {
-
         var knownData = new Buffer(15);
-        knownData[0] = 0x03; // Length
-        knownData[1] = 0x03; // Parameter: Service List
-        knownData[2] = 0xD8; // URI Beacon ID
-        knownData[3] = 0xFE; // URI Beacon ID
-        knownData[4] = 0x0A; // Length
-        knownData[5] = 0x16; // Service Data
-        knownData[6] = 0xD8; // URI Beacon ID
-        knownData[7] = 0xFE; // URI Beacon ID
-        knownData[8] = 0x00; // Flags
-        knownData[9] = 0xEB; // Power
-        knownData[10] = 0x00; // http://www.
-        knownData[11] = 0x61; // a
-        knownData[12] = 0x62; // b
-        knownData[13] = 0x63; // c
-        knownData[14] = 0x07; // .com
+
+        // Service List
+        knownData.writeUInt8(3, 0); // length
+        knownData.writeUInt8(0x03, 1); // GAP Service List type
+        knownData.writeUInt16LE(0xFEAA, 2); // GAP Service List value, UUID 0xFEAA
+
+        // Service Data
+        knownData.writeUInt8(10, 4); // length
+        knownData.writeUInt8(0x16, 5); // GAP Service Data type
+        knownData.writeUInt16LE(0xFEAA, 6); // GAP Service Data UUID 0xFEAA
+
+        //  Beacon data
+        knownData.writeUInt8(0x10, 8); // flags
+        knownData.writeInt8(-21, 9); // power
+
+        knownData.writeUInt8(0x00, 10); // http://www.
+        knownData.write("abc", 11);
+        knownData.writeUInt8(0x07, 14); // .com
 
         var advertisementData = uriBeacon.makeBuffer("http://www.abc.com");
         assert.deepEqual(knownData, advertisementData);
     });
 
     it('should allow tx power level to be set', function () {
-
         var knownData = new Buffer(15);
-        knownData[0] = 0x03; // Length
-        knownData[1] = 0x03; // Parameter: Service List
-        knownData[2] = 0xD8; // URI Beacon ID
-        knownData[3] = 0xFE; // URI Beacon ID
-        knownData[4] = 0x0A; // Length
-        knownData[5] = 0x16; // Service Data
-        knownData[6] = 0xD8; // URI Beacon ID
-        knownData[7] = 0xFE; // URI Beacon ID
-        knownData[8] = 0x00; // Flags
-        knownData[9] = 0x9C; // Power
-        knownData[10] = 0x00; // http://www.
-        knownData[11] = 0x65; // e
-        knownData[12] = 0x66; // f
-        knownData[13] = 0x66; // f
-        knownData[14] = 0x08; // .org
+
+        // Service List
+        knownData.writeUInt8(3, 0); // length
+        knownData.writeUInt8(0x03, 1); // GAP Service List type
+        knownData.writeUInt16LE(0xFEAA, 2); // GAP Service List value, UUID 0xFEAA
+
+        // Service Data
+        knownData.writeUInt8(10, 4); // length
+        knownData.writeUInt8(0x16, 5); // GAP Service Data type
+        knownData.writeUInt16LE(0xFEAA, 6); // GAP Service Data UUID 0xFEAA
+
+        //  Beacon data
+        knownData.writeUInt8(0x10, 8); // flags
+        knownData.writeInt8(-100, 9); // power
+
+        knownData.writeUInt8(0x00, 10); // http://www.
+        knownData.write("eff", 11);
+        knownData.writeUInt8(0x08, 14); // .org
 
         var advertisementData = uriBeacon.makeBuffer("http://www.eff.org", -100);
         assert.deepEqual(knownData, advertisementData);
