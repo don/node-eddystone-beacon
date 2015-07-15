b4e5d0a10832f4743684926a307634842f9f8fc9
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-15 16:01:55 -0400

    add GAP flags to "raw" bleno examples

--- examples/blenoBeacon.js
+++ examples/blenoBeacon.js
@@ -1,23 +1,26 @@
 // Manually create a Eddystone Beacon with bleno
 var bleno = require('bleno');
 
-var advertisementData = new Buffer(15); // maximum 31 bytes
+var advertisementData = new Buffer(18); // maximum 31 bytes
 
-advertisementData[0] = 0x03; // Length
-advertisementData[1] = 0x03; // Parameter: Service List
-advertisementData[2] = 0xAA; // Eddystone Beacon ID
-advertisementData[3] = 0xFE; // Eddystone Beacon ID
-advertisementData[4] = 0x0A; // Length
-advertisementData[5] = 0x16; // Service Data
-advertisementData[6] = 0xAA; // Eddystone Beacon ID
-advertisementData[7] = 0xFE; // Eddystone Beacon ID
-advertisementData[8] = 0x10; // Flags
-advertisementData[9] = 0xEE; // Power -18 dBm
-advertisementData[10] = 0x00; // http://www.
-advertisementData[11] = 0x61; // a
-advertisementData[12] = 0x62; // b
-advertisementData[13] = 0x63; // c
-advertisementData[14] = 0x07; // .com
+advertisementData[0] = 0x02; // Length
+advertisementData[1] = 0x01; // Parameter: Flags
+advertisementData[2] = 0x06;
+advertisementData[3] = 0x03; // Length
+advertisementData[4] = 0x03; // Parameter: Service List
+advertisementData[5] = 0xAA; // Eddystone Beacon ID
+advertisementData[6] = 0xFE; // Eddystone Beacon ID
+advertisementData[7] = 0x0A; // Length
+advertisementData[8] = 0x16; // Service Data
+advertisementData[9] = 0xAA; // Eddystone Beacon ID
+advertisementData[10] = 0xFE; // Eddystone Beacon ID
+advertisementData[11] = 0x10; // Flags
+advertisementData[12] = 0xEE; // Power -18 dBm
+advertisementData[13] = 0x00; // http://www.
+advertisementData[14] = 0x61; // a
+advertisementData[15] = 0x62; // b
+advertisementData[16] = 0x63; // c
+advertisementData[17] = 0x07; // .com
 
 bleno.on('stateChange', function(state) {
     if (state === 'poweredOn') {
--- examples/flexibleBeacon.js
+++ examples/flexibleBeacon.js
@@ -4,21 +4,24 @@
 var EddystoneBeacon = require('./../eddystone-beacon'),
     bleno = require('bleno');
 
-var template = new Buffer(10); // maximum 31 bytes
-template[0] = 0x03; // Length
-template[1] = 0x03; // Parameter: Service List
-template[2] = 0xAA; // Eddystone Beacon ID
-template[3] = 0xFE; // Eddystone Beacon ID
-template[4] = 0x00; // Length <-- must be updated
-template[5] = 0x16; // Service Data
-template[6] = 0xAA; // Eddystone Beacon ID
-template[7] = 0xFE; // Eddystone Beacon ID
-template[8] = 0x10; // Flags
-template[9] = 0xEC; // Power -20 dBm
+var template = new Buffer(13); // maximum 31 bytes
+template[0] = 0x02; // Length
+template[1] = 0x01; // Parameter: Flags
+template[2] = 0x06;
+template[3] = 0x03; // Length
+template[4] = 0x03; // Parameter: Service List
+template[5] = 0xAA; // Eddystone Beacon ID
+template[6] = 0xFE; // Eddystone Beacon ID
+template[7] = 0x00; // Length <-- must be updated
+template[8] = 0x16; // Service Data
+template[9] = 0xAA; // Eddystone Beacon ID
+template[10] = 0xFE; // Eddystone Beacon ID
+template[11] = 0x10; // Flags
+template[12] = 0xEC; // Power -20 dBm
 
 var encoded = EddystoneBeacon.encodeUrl("http://example.com");
-var advertisementData = Buffer.concat([template, encoded], template.length + encoded.length);
-advertisementData[4] = encoded.length + 5;
+var advertisementData = Buffer.concat([template, encoded]);
+advertisementData[7] = encoded.length + 5;
 
 bleno.on('stateChange', function(state) {
     if (state === 'poweredOn') {
