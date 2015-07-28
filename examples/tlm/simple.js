e22dba8d2f64e58ee70cec867433ec5e0e666c2f
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-28 11:34:47 -0400

    random data in TLM simple example

--- examples/tlm/simple.js
+++ examples/tlm/simple.js
@@ -3,3 +3,11 @@
 var eddystoneBeacon = require('./../../index');
 
 eddystoneBeacon.advertiseTlm();
+
+setInterval(function() {
+    var randomBatteryVoltage = Math.floor((Math.random() * 10000) + 500); // between 500 and 10,000
+    var randomTemperature = (Math.random() * 256.0) - 128.0; // between -128.0 and 128.0
+
+    eddystoneBeacon.setBatteryVoltage(randomBatteryVoltage);
+    eddystoneBeacon.setTemperature(randomTemperature);
+}, 10 * 1000);
