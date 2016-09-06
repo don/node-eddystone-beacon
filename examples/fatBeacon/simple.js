eb27b3bb9e32a87c866bb31059144005e20b632c
Ben Hardill <hardillb@gmail.com>
2016-09-06 09:42:58 +0000

    Get basic fat beacon working

--- examples/fatBeacon/simple.js
+++ examples/fatBeacon/simple.js
@@ -4,12 +4,14 @@ var eddystoneBeacon = require('./../../index');
 
 var html = "<html>" + 
       "<head>" +
-       "<title>Fat Beacon Demo</title>" +
-       "<meta name='description' content='FatBeacon Demo'/>"
-      "</head>" + 
-      "<body>" +
-       "<h1>HelloWorld</h1>"
-      "</body>" + 
-       "</html>";
+        "<title>Fat Beacon Demo2</title>" +
+        "<meta charset='UTF-8'>" + 
+        "<meta name='description' content='Fat Beacon Demo'/>" + 
+       "</head>" + 
+       "<body>" +
+        "<h1>HelloWorld</h1>" +
+        "This is a test" + 
+       "</body>" + 
+      "</html>";
 
 eddystoneBeacon.advertiseFatBeacon('Fat Beacon Demo', {html: html});
