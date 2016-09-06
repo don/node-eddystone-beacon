765c1fa769017b57956d5ef6243a346d733e6fae
Ben Hardill <hardillb@gmail.com>
2016-09-06 15:50:43 +0100

    Fix for payloads large than 1 MTU

--- examples/fatBeacon/simple.js
+++ examples/fatBeacon/simple.js
@@ -4,13 +4,16 @@ var eddystoneBeacon = require('./../../index');
 
 var html = "<html>" + 
       "<head>" +
-        "<title>Fat Beacon Demo2</title>" +
+        "<title>Fat Beacon Demo</title>" +
         "<meta charset='UTF-8'>" + 
         "<meta name='description' content='Fat Beacon Demo'/>" + 
        "</head>" + 
        "<body>" +
         "<h1>HelloWorld</h1>" +
         "This is a test" + 
+        "<div>Adding more content to see if I can it over the MTU value</div>" + 
+        "<div>Adding more content to see if I can it over the MTU value</div>" + 
+        "<div>Adding more content to see if I can it over the MTU value</div>" + 
        "</body>" + 
       "</html>";
 
